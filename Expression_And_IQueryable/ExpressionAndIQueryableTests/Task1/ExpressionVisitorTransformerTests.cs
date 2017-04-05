33ae26d3daabf4c4463741bda4da6d81fd637636
Ivan Shymanouski <Ivan_Shymanouski@epam.com>
2017-04-05 22:07:24 +0300

    EAI

---
+++ Expression_And_IQueryable/ExpressionAndIQueryableTests/Task1/ExpressionVisitorTransformerTests.cs
@@ -0,0 +1,56 @@
+﻿using ExpressionAndIQueryable.Task1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace ExpressionAndIQueryableTests.Task1
+{
+    [TestClass]
+    public class ExpressionVisitorTransformerTests
+    {
+        [TestMethod]
+        public void TransformToIncrementTest()
+        {
+            Expression<Func<double, double>> source = (x) => x + 1.0;
+
+            var actual = new ExpressionVisitorTransformer().VisitAndConvert(source, "");
+
+            Assert.AreEqual("x => Increment(x)", actual?.ToString());
+        }
+
+        [TestMethod]
+        public void TransformToDecrementTest()
+        {
+            Expression<Func<int, int>> source = (x) => x - 1;
+
+            var actual = new ExpressionVisitorTransformer().VisitAndConvert(source, "");
+
+            Assert.AreEqual("x => Decrement(x)", actual?.ToString());
+        }
+
+        [TestMethod]
+        public void NoTransformationTest()
+        {
+            Expression<Func<int, int>> source = (x) => x - 2;
+
+            var actual = new ExpressionVisitorTransformer().VisitAndConvert(source, "");
+
+            Assert.AreEqual("x => (x - 2)", actual?.ToString());
+        }
+
+        [TestMethod]
+        public void ParameterIsExistExpression()
+        {
+            Expression<Func<int, int, int, int>> source = (a, b, c) => a + b + c;
+
+            var actual = new ExpressionVisitorTransformer().Transform(source, new Dictionary<string, object>
+            {
+                { "a", 1993 },
+                { "c", 2020 }
+            });
+
+            Assert.AreEqual("b => 1993 + b + 2020", actual?.ToString().Replace("(", "").Replace(")",""));
+        }
+    }
+}
---
+++ Expression_And_IQueryable/ExpressionAndIQueryableTests/Task3/TelescopeTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Configuration;
+using System.Linq;
+using ExpressionAndIQueryble.E3SClient.Entities;
+
+namespace ExpressionAndIQueryble
+{
+    [TestClass]
+    public class TelescopeTest
+    {
+        [TestMethod]
+        public void SwapEqualStatementElementsTest()
+        {
+            var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+
+            foreach (var emp in employees.Where(e => "EPBYMINW5387" == e.workstation))
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.office);
+            }
+        }
+
+        [TestMethod]
+        public void StringEndsWithTest()
+        {
+            var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+
+            foreach (var emp in employees.Where(e => e.workstation.EndsWith("5387")))
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.office);
+            }
+        }
+
+        [TestMethod]
+        public void StringStartWithTest()
+        {
+            var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+
+            foreach (var emp in employees.Where(e => e.workstation.StartsWith("EPBYMINW53")))
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.office);
+            }
+        }
+
+        [TestMethod]
+        public void StringContainsTest()
+        {
+            var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+
+            foreach (var emp in employees.Where(e => e.workstation.Contains("NW538")))
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.office);
+            }
+        }
+
+        [TestMethod]
+        public void StatementAndTest()
+        {
+            var employees = new E3SEntitySet<EmployeeEntity>(ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+
+            foreach (var emp in employees.Where(e => "EPBYMINW5387" == e.workstation && e.nativename.StartsWith("Ив")))
+            {
+                Console.WriteLine("{0} {1}", emp.nativename, emp.office);
+            }
+        }
+    }
+}
